ba7070f01d24f6d9f04b68a851f61fd2f45e2583
Florian <floriangeorges1992@gmail.com>
2024-11-25 21:18:17 +0100

    update index and add scintifique.js file

---
+++ src/index.js
@@ -0,0 +1,6 @@
+//déclarer un nombre
+const monnombre = 45; //litéral
+
+console.log(monnombre);
+console.log(Number(85)); //fonction
+console.log(new Number(85)); // instanciation objet number
---
+++ src/scientifique.js
@@ -0,0 +1,11 @@
+//declarer un grand nombre
+const prix = 1e6;
+console.log(prix);
+console.log(1 + +"44");
+
+//ParseInt
+console.log(+"44");
+console.log(parseInt("11"));
+
+//ParseFloat
+console.log(parseFloat("11.55"));
